820c1979446fdd318169444a6328f3b48dd4faab
MBashov <galacticos.mb@gmail.com>
2025-01-16 17:11:07 +0100

    Add movie controller and create page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import homeControler from './controllers/homeController.js';
+import movieController from './controllers/movieController.js';
 
 const routes = Router();
 routes.use(homeControler);
+routes.use(movieController);
 
 routes.get('*', (req, res) => {
     res.render('404');
