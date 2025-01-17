df9a67bba5ebc8465237eff8cf78ae8986476fd5
MBashov <galacticos.mb@gmail.com>
2025-01-17 19:57:24 +0100

    Add search by genre and year

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -9,6 +9,16 @@ export default {
         if (filter.search) {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
         }
+
+        if (filter.genre) {
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+        }
+
+        if (filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
+
+        return result;
     },
 
     findOne(movieId) {
