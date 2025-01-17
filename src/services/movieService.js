6471ab672f4a0d7fe214e16bb9c10e7becaec324
MBashov <galacticos.mb@gmail.com>
2025-01-17 14:20:12 +0100

    Add getAll to movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,6 +2,11 @@ import { v4 as uuid } from 'uuid';
 import movies from "../movies.js";
 
 export default {
+
+    getAll() {
+        return movies;
+    },
+
     findOne(movieId) {
         //TODO : If movie is missing?
         const movie = movies.find(movie => movie.id === movieId);
