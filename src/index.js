45f06dcfb73da3fd7b398d9c6a2238ee551a291f
MBashov <galacticos.mb@gmail.com>
2025-01-23 15:25:17 +0100

    Create movie model

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
 
 import routes from './routes.js'
 import showRatingHelper from './helpers/ratingHelpers.js';
 
 const app = express();
 
+// db configuration
+try {
+    await mongoose.connect('mongodb://localhost:27017/magic-movies-jan-2025');
+    console.log('DB Conected Successfuly!');
+} catch (error) {
+    console.log('Cannot conect to DB!');
+    console.error(error.message);
+}
+
+// handlebars configuration
 app.engine('hbs', handlebars.engine({
     'extname': 'hbs',
     helpers: {
@@ -15,9 +26,12 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
 
+// express configuration
 app.use('/static', express.static('src/public'));
 app.use(express.urlencoded({ extended: false })); // Learn express to parse formdata    
 
+// setup routes
 app.use(routes);
 
+// start server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
