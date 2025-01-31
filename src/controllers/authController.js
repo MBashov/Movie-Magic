9096b9ca41f32144e00de862f1a16e81304607f6
MBashov <galacticos.mb@gmail.com>
2025-01-31 09:41:29 +0100

    Send data to register endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,14 @@ const authController = Router();
 
 authController.get('/register', (req, res) => {
     res.render('auth/register');
-    
 });
 
+authController.post('/register', async (req, res) => {
+    
+    const userData = req.body;
+    
+
+    
+    res.end();
+});
 export default authController;
