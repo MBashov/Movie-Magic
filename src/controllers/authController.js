e6e6a432cfd305fa980ce70cf0f48673dc1b3f86
MBashov <galacticos.mb@gmail.com>
2025-01-31 10:43:10 +0100

    Add login functionality with jwt generator

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-import { Router } from "express"; 
+import { Router } from "express";
 import authService from "../services/authService.js";
 
 const authController = Router();
@@ -8,16 +8,32 @@ authController.get('/register', (req, res) => {
 });
 
 authController.post('/register', async (req, res) => {
-    
+
     const userData = req.body;
-    
+
     await authService.register(userData);
-    
+
     res.redirect('/');
 });
 
 authController.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+authController.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+        console.log(token);
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.redirect('/404');
+    }
+
+    res.redirect('/');
 });
 
 export default authController;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,36 @@
 import User from "../models/User.js"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-export default{
+const SECRET = '$1w$10$1KTze.DQ8FnS86EhYI9D2eX3vGbuCCyDjQaWtpiw/T8MwdO2QJ1rS';
+
+export default {
+
     register(userData) {
         return User.create(userData);
+    },
+
+    async login(email, password) {
+        // Check if user exist
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('Invalid email or password!');
+        }
+
+        // Check if password is correct
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            throw new Error('Invalid password or password!');
+        }
+
+        // Generate token
+        const paylod = {
+            id: user.id,
+            email: user.email
+        }
+        // TODO: Use async function
+        const token = jwt.sign(paylod, SECRET, { expiresIn: '2h' });
+        return token;
     }
+
 }
