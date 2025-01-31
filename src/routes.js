6929e5c8833a443a86d252ce2f8004bd0447d6cc
MBashov <galacticos.mb@gmail.com>
2025-01-31 09:31:56 +0100

    Add auth controller

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import homeControler from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castContoroller.js';
+import authController from './controllers/authController.js';
 
 const routes = Router();
 
 routes.use(homeControler);
 routes.use('/movies', movieController);
 routes.use('/casts', castController)
+routes.use('/auth', authController)
 
 routes.get('*', (req, res) => {
     res.render('404');
