e6e56c511ad105c3898b937eb6f94d2dfeb2c99e
MBashov <galacticos.mb@gmail.com>
2025-01-31 10:07:46 +0100

    Hash password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchrema = new Schema({
     'email': String,
     'password': String,
+});
+
+userSchrema.pre('save', async function () {
+    //TODO: Fix update user bug
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = model('User', userSchrema);
