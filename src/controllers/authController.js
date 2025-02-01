db725ac88dfdccda2bf4dba5b6973715e426e4bd
MBashov <galacticos.mb@gmail.com>
2025-02-01 08:15:32 +0100

    Add logout functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -35,4 +35,9 @@ authController.post('/login', async (req, res) => {
 
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
+
 export default authController;
