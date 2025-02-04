33e07dcc5a6e02a0dff3048ebb668e97e3e4654a
MBashov <galacticos.mb@gmail.com>
2025-02-04 14:20:48 +0100

    Add movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,14 +2,47 @@ import { Schema, model, Types } from 'mongoose';
 
 // Create Schema
 const movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [2, 'Title should be at least 2 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Title should be alphanumeric, digits and whitespaces only!'],
+    },
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [2, 'Genre should be at least 2 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Genre should be alphanumeric, digits and whitespaces only!'],
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required!'],
+        minLength: [2, 'Director should be at least 2 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Director should be alphanumeric, digits and whitespaces only!'],
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required!'],
+        min: 1900,
+        max: 2028
+    },
+    imageUrl: {
+        type: String,
+        match: /^https?:\/\//
+    },
+    rating: {
+        type: Number,
+        required: [true, 'Rating is required!'],
+        min: 0,
+        max: 10
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: [20, 'Description should be at least 20 characters long!'],
+        match: [/^[a-zA-Z0-9\s]+$/, 'Description should be alphanumeric, digits and whitespaces only!']
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
